6f70f4c0ce22b1d60a5b5b574197b0467ce7af7a
Leva <petrosian.lyova@yandex.ru>
2018-11-26 00:06:00 +0500

    Петросян. Task07

--- task07/src/com/example/task07/Processor.java
+++ task07/src/com/example/task07/Processor.java
@@ -1,9 +1,12 @@
 package com.example.task07;
 
+import java.io.IOException;
+
 public class Processor {
 
     public Object process() throws Exception {
-        //todo вы можете заменить реализацию этого метода для ручного дебага
+
+        //throw new IOException();
         return null;
     }
 
