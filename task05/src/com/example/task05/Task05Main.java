b4dff4be218bf4f622d0c9460acc5748fb22d19b
Nikita Sviridov <n.sviridov14@gmail.com>
2018-11-27 17:03:29 +0500

    Sviridov Nikita task05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,29 +1,36 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Task05Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String pathToFile = args[0]; // "/home/user/file.txt"
 
         String s = readFile(pathToFile);
         System.out.println(s);
     }
 
-    public static String readFile(String pathToFile) throws IOException {
-        FileReader fileReader = new FileReader(pathToFile);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
+    public static String readFile(String pathToFile) {
+        try {
+            FileReader fileReader = new FileReader(pathToFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        StringBuilder stringBuilder = new StringBuilder();
-        String currentLine;
-        while ((currentLine = bufferedReader.readLine()) != null) {
-            stringBuilder.append(currentLine);
-            stringBuilder.append("\n");
+            StringBuilder stringBuilder = new StringBuilder();
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(currentLine);
+                stringBuilder.append("\n");
+            }
+            bufferedReader.close();
+
+            return stringBuilder.toString();
+        } catch (FileNotFoundException e) {
+           return "файл " + pathToFile + " не найден";
+        } catch (IOException e) {
+            return "произошла ошибка при чтении файла" + pathToFile;
         }
-        bufferedReader.close();
-
-        return stringBuilder.toString();
     }
 }
