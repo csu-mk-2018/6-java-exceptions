61166ed8753f1d92770fd3eae99f4c2280a487ef
BMSalamander <hostan3@gmail.com>
2018-12-01 17:55:43 +0500

    Антипьев Александр. Задание 03.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -4,13 +4,18 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        throwCheckedException();
-         */
+        try {
+            throwCheckedException();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ex");
+        }
+
     }
 
     //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
-
+    public static void throwCheckedException() throws Exception {
+        throw new Exception("exception");
     }
 }
