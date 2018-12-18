00cbb5390eb460109c5f49a9ab9a54142cc52fd5
Alexander <alex.goryushkin@gmail.com>
2018-12-18 19:39:23 +0500

    Горюшкин Александр. Задание 05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,35 +1,36 @@
 package com.example.task05;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Task05Main {
     public static void main(String[] args) {
-        String pathToFile = args[0]; // "/home/user/file.txt"
+        String pathToFile = args[0];
 
-        String s = readFile(pathToFile);
-        System.out.println(s);
+        try {
+            String s = readFile(pathToFile);
+            System.out.println(s);
+        }
+        catch (FileNotFoundException e){
+            System.out.print(String.format("файл \"%s\" не найден\n", pathToFile));
+        }
+        catch (IOException e) {
+            System.out.println(String.format("произошла ошибка при чтении файла \"%s\"", pathToFile));
+        }
     }
 
-    public static String readFile(String pathToFile) {
-        try (FileReader fileReader = new FileReader(pathToFile); BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-
-            StringBuilder stringBuilder = new StringBuilder();
-            String currentLine;
-
-            while ((currentLine = bufferedReader.readLine()) != null) {
-                stringBuilder.append(currentLine);
-                stringBuilder.append("\n");
-            }
-
-            return stringBuilder.toString();
-
-        } catch (FileNotFoundException ex) {
-            return String.format("файл \"%s\" не найден", pathToFile);
-        } catch (IOException e) {
-            return String.format("произошла ошибка при чтении файла \"%s\"", pathToFile);
+    public static String readFile(String pathToFile) throws IOException {
+        FileReader fileReader = new FileReader(pathToFile);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuilder stringBuilder = new StringBuilder();
+        String currentLine;
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            stringBuilder.append(currentLine);
+            stringBuilder.append("\n");
         }
+        bufferedReader.close();
+        return stringBuilder.toString();
     }
 }
